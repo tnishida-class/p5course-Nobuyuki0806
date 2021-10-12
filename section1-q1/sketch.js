8ce83f124f00ed0e2b61adfa84cfa8dc941070c1
Nobuyuki0806 <65203263+Nobuyuki0806@users.noreply.github.com>
2021-10-12 12:52:36 +0900

    update_section1-q1

--- section1-q1/sketch.js
+++ section1-q1/sketch.js
@@ -5,10 +5,14 @@ function setup(){
 
   // 空欄を埋めて神戸市のロゴマークを完成させよう
   noFill();
+  // 塗りつぶしなし
   strokeWeight(25);
+  // 線の幅
   strokeCap(SQUARE);
+  // 線の形　ROUNDやPROJECTなど
   arc(100 + 25, 100, 100, 100, QUARTER_PI * 3, QUARTER_PI * 3 + PI);
-  // BLANK[1]
+  // (中心のx座標, y座標, 横幅, 縦幅, 開始角度, 終了角度) 角度は弧度法で、QUARTER_PI=π/4
+  arc(100 - 25, 100, 100, 100, QUARTER_PI + PI, QUARTER_PI + PI + PI);
 
   // おまけ：神戸大学のロゴを作りたい人向けに色情報だけ置いておきます
   const red = color(196, 0, 0);
