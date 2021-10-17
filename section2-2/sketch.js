6641183cb6994fb7e7fd7f90ec12909e0ac62f46
Nobuyuki0806 <65203263+Nobuyuki0806@users.noreply.github.com>
2021-10-17 19:16:11 +0900

    changes_section2

--- section2-2/sketch.js
+++ section2-2/sketch.js
@@ -8,14 +8,16 @@ function setup() {
   createCanvas(100,100);
   background(196);
   for(let i = 0; i < 9; i++){
-    // 変えるのはここから
-    if(i % 2 == 0){
+//     // 変えるのはここから
+    if(i%3==0){
+      strokeWeight(3);
+    }
+    else if(i%3==1){
       strokeWeight(2);
     }
     else{
       strokeWeight(1);
     }
-    // ここまでの間になります
     let x = i * 10 + 10;
     line(x, 0, x, 100);
   }
