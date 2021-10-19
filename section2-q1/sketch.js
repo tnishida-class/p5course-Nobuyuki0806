79709a1affb20a894419f0fc7ff3ec14dfc781b4
Nobuyuki0806 <65203263+Nobuyuki0806@users.noreply.github.com>
2021-10-19 12:18:06 +0900

    changes_section2_q12

--- section2-q1/sketch.js
+++ section2-q1/sketch.js
@@ -1,6 +1,15 @@
 // 小手調べ
 function setup(){
+  createCanvas(1000,1000);
   for(let i = 0; i < 10; i++){
-    // BLANK[1]
+    noFill();
+    if(i<5){
+      stroke(0,0,225);
+    }
+    else{
+      stroke(255,0,0);
+    }
+    let x = i*50+50;
+    ellipse(500,500,x);
   }
 }
--- section2-q2/sketch.js
+++ section2-q2/sketch.js
@@ -1,11 +1,37 @@
 // チェッカー
 function setup() {
   createCanvas(200, 200);
+  let width=200;
+  // これを入れないとwidthが何か分からない？
   let size = width / 8;
   noStroke();
   for(let i = 0; i < 8; i++){
     for(let j = 0; j < 8; j++){
       // BLANK[1] (hint: rectのx座標は size * i, y座標はsize * j)
+      if((i+j)%2==0){
+        // ()をつけないとjを2で割ったあまりにiを加えることになるのでおかしくなる
+        fill(255);
+      }
+      else{
+        fill(192,192,192);
+      }
+
+      rect(size*i,size*j,size,size);
+        // (左上のx座標, 左上のy座標, 四角形の幅, 四角形の高さ)
+
+      if((i+j)%2==1&&j<3){
+        fill(255,0,0);
+        ellipse(size*i+size/2,size*j+size/2,size-5)
+        // sizeは適当
+      }
+      else if((i+j)%2==1&&j>4){
+        fill(0,0,0);
+        ellipse(size*i+size/2,size*j+size/2,size-5)
+        // sizeは適当
+      }
+
     }
-  }
+
+    }
+
 }
