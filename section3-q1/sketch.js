8f2e53ef1b94724c6210efc0c9110647b0022f2b
Nobuyuki0806 <65203263+Nobuyuki0806@users.noreply.github.com>
2021-11-05 15:44:33 +0900

    cahnges_section3_q1

--- section3-q1/sketch.js
+++ section3-q1/sketch.js
@@ -8,9 +8,13 @@ function setup(){
   cycle = 120;
 }
 // 心臓はポンプの役割をした後一瞬止まっている
-function draw(){
-  background(160, 192, 255);
-  count = (count + 1) % cycle;
+
+
+  function draw(){
+
+if(mouseIsPressed){
+    background(160, 192, 255);
+    count = (count + 2) % cycle;
   // BLANK[1]
   if(count<20){
     ellipse(width / 2, height / 2, 20)}
@@ -20,4 +24,25 @@ function draw(){
     ellipse(width / 2, height / 2, 100-count)}
   else{
     ellipse(width / 2, height / 2, 20)}
-}
+
+
+    }
+
+
+else{
+
+    background(160, 192, 255);
+    count = (count + 1) % cycle;
+    // BLANK[1]
+    if(count<20){
+      ellipse(width / 2, height / 2, 20)}
+    else if(count<50){
+      ellipse(width / 2, height / 2, count)}
+    else if(count<80){
+      ellipse(width / 2, height / 2, 100-count)}
+    else{
+      ellipse(width / 2, height / 2, 20)}
+
+    }
+
+  }
