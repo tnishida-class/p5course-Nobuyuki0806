f016c4e2f795fc60e636a7b2c5a68f6b6e826880
Nobuyuki0806 <65203263+Nobuyuki0806@users.noreply.github.com>
2021-11-05 16:35:15 +0900

    changes_section3_2

--- section3-2/sketch.js
+++ section3-2/sketch.js
@@ -13,46 +13,30 @@ function setup(){
 
 function draw(){
   background(160, 192, 255);
-  ellipse(x, y, 20, 20);
-  x += vx;
-  y += vy;
+  ellipse(x, y*2-10, 20, 20);
+　// y*2-10でぴったり下についている状態
+  y=y+vy
+  y=constrain(y,0,height)
 
-  // 重力（コメント機能でオンオフ切り替えて実行してみましょう）
-  vy = constrain(vy + g, -vyMax, vyMax);
+  if(y>y*2-10){y=y*2-10}
+  if(keyIsDown(RIGHT_ARROW)){
+    x+=3;
+  }
+  if(keyIsDown(LEFT_ARROW)){
+    x-=3;
+  }
+  if(keyIsDown(UP_ARROW)){
 
-  // 端の処理パターン (1) 反対側から出てくる
-  // if(x > width){ x = 0; }
-  // else if(x < 0){ x = width; }
-  // if(y > height){ y = 0; }
-  // if(y < 0){ y = height; }
+    y=y-vy+g;
+  }
+  }
 
-　// 端の処理パターン (2) 跳ね返る
-  if(x < 0 || x > width){ vx = -1 * vx; }
-  if(y > height){ vy = -1 * vy; }
-  x = constrain(x, 0, width);
-  y = constrain(y, 0, height);
-}
+
 
 function windowResized(){
   resizeCanvas(windowWidth, windowHeight);
 }
 
-// let x, y; // ポイント1：関数の外で変数を宣言する
-//
-// function setup(){
-//   createCanvas(windowWidth, windowHeight);
-//   x = width / 2;
-//   y = height / 2;
-// }
-//
-// function draw(){
-//   background(160, 192, 255); // ポイント2: draw の最初に background を書く
-//   ellipse(x, y, 30);
-//   x += 2;
-//   y -= 2;
-//
-//   if(x>width){x=0}
-//   else if(x<0){x=width}
-//   if(y>height){y=0}
-//   else if(y<0){y=height}
-// }
+function keyPressed(){
+  if(keyCode==UP_ARROW){y=y-vy+g}
+}
