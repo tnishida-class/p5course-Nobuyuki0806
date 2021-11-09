e26b004f452d98aa2cb9f5329c0ec0c977f4c29c
Nobuyuki0806 <65203263+Nobuyuki0806@users.noreply.github.com>
2021-11-09 12:01:09 +0900

    changes_section4_q2

--- section4-q2/sketch.js
+++ section4-q2/sketch.js
@@ -9,10 +9,17 @@ function setup(){
   for(let i = 0; i < 10; i++){
     scores[i] = random(20, 100); // 60以上100未満のランダムな数を代入
   }
-
+  console.log(scores);
   // 円グラフを描くには割合が必要なので合計を計算しておく
   let total = 0;
   for(let i = 0; i < scores.length; i++){ total += scores[i]; }
-
+  console.log(total)
   // BLANK[1]
+  ellipse(200,200,300);
+  for(let i=0;i<scores.length;i++){
+    // fill(240)
+    // arc(200,200,300,300,PI*scores[i]/total,PI*scores[i+1]/total,true);
+    line(200,200,200+150*cos(2*PI*scores[i]/total),200+150*sin(2*PI*scores[i]/total))
+    // line(200,200,200+150*cos(PI*scores[i+1]/total),200+150*sin(PI*scores[i+1]/total))
+  }
 }
