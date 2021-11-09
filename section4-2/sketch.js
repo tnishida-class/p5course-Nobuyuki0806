9b4f7c3111dacf9a07cb63f4d1d1ab5b7182b41c
Nobuyuki0806 <65203263+Nobuyuki0806@users.noreply.github.com>
2021-11-10 01:43:13 +0900

    Update sketch.js

--- section4-2/sketch.js
+++ section4-2/sketch.js
@@ -11,6 +11,8 @@ function setup(){
 
 function draw(){
   background(160, 192, 255);
+  let x=width/2,y=height/2;
+  ellipse(x,y,random(20, 100));
   for(let i = 0; i < balls.length; i++){
     let b = balls[i];
     ellipse(b.x, b.y, b.size);
@@ -23,7 +25,7 @@ function mouseDragged(){
   const dx = mouseX - pmouseX;
   const dy = mouseY - pmouseY;
   if(mag(dx, dy) > 5){
-    const b = { x: mouseX, y: mouseY, size: 20, vx: dx, vy: dy };
+    const b = { x: mouseX, y: mouseY, size: random(20, 100), vx: dx, vy: dy };
     balls.push(b);
   }
 }
